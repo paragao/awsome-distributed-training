d22e1bd55ce49d681de9c6c04a3ba2b34c9c8586
Matthew Nightingale <73195085+nghtm@users.noreply.github.com>
2025-02-09 23:53:19 -0800

    Update restart-slurmd.sh

--- 1.architectures/5.sagemaker-hyperpod/restart-slurmd.sh
+++ 1.architectures/5.sagemaker-hyperpod/restart-slurmd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Node list as a space-separated array
-node_list= " "
-nodes=$(scontrol show hostname $node_list)
+$NODE_LIST=""
+nodes=$(scontrol show hostname "$NODE_LIST")
 
 # Loop through each node and restart slurmd
 for node in $nodes; do
     echo "Restarting slurmd on $node"
-    ssh $node "sudo systemctl restart slurmd"
+    ssh -o StrictHostKeyChecking=no$node "sudo systemctl restart slurmd"
 done
